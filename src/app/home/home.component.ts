1ebfa62d0bf10b26ab56869475664c1ce5613cc3
adarshperi <adarshperi@gmail.com>
2022-08-23 20:37:44 +0530

    minor changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public modal: ModalService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  openModal($event: Event) {
+    $event.preventDefault();
+    this.modal.toggleModal('auth');
+  }
 }
